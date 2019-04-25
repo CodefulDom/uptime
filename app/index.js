86fb3f551eb3a2a7f0cbc356e52f732b43f6c052
codefuldom <dominiquehallan@gmail.com>
2019-04-25 13:23:56 -0500

    adding parser

--- app/index.js
+++ app/index.js
@@ -4,10 +4,14 @@ Primary file for API
 
 // Deps
 const http = require('http')
+const url = require('url')
 
-// server should respond to all requests with a string
-const server = http.createServer((req, res) => {
-  res.end('Hello, World\n')
+const server = http.createServer(function(req, res) {
+  let parsedURL = url.parse(req.url, true)
+  let path = parsedURL.pathname
+  let trimmedPath = path.replace(/^\/+|\/+$/g, '')
+
+  res.end(`Request recieved on path: ${trimmedPath}\n`)
 })
 
 // start the server!
